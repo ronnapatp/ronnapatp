294d35ca2a24094a6f8ba58dfb8bb43a302624ff
Ronnapatp <84321166+ronnapatp@users.noreply.github.com>
2021-08-13 19:39:43 +0700

    Add files via upload

---
+++ writeme.rb
@@ -0,0 +1,43 @@
+require "erb"
+require "httparty"
+require "nokogiri"
+
+
+tdl_res = HTTParty.get("https://raw.githubusercontent.com/ronnapatp/today-I-learn/main/README.md")
+tdl_entries = tdl_res.body.strip.split("\n").select { |l| l.start_with?("- ") }.size
+
+witq_res = HTTParty.get("https://raw.githubusercontent.com/ronnapatp/what-I-eat-today/main/README.md")
+witq_entries = witq_res.body.strip.split("\n").select { |l| l.start_with?("- ") }.size
+
+it_res = HTTParty.get("https://raw.githubusercontent.com/ronnapatp/intheofficefor/main/Readme.md")
+it_entries = it_res.body.strip.split("\n").select { |l| l.start_with?("- ") }.size
+
+@active_projects = [
+  { name: "Kaolad bot V2.0", link: "https://github.com/ronnapatp/kaoladbot" },
+  { name: "World education", link: "https://github.com/ronnapatp/worldeducation" },
+  { name: "Smart team website", link: "https://github.com/ronnapatp/smartteam-website" },
+  { name: "การศึกษาไทยเท่าที่เล่าได้", link: "https://thedu.vercel.app/" },
+  { name: "กินอะไรดี? ver.Thai", link: "https://todayfood.vercel.app/", duration: "#{witq_entries} menu" },
+  { name: "กินอะไรดี ver.Eng", link: "https://today-food-eng.vercel.app/" },
+  {
+    name: "Today I learned",
+    link: "https://github.com/ronnapatp/today-I-learn/blob/main/README.md",
+    duration: "#{tdl_entries} Posts",
+  },
+]
+
+@past_projects = [
+  {
+    name: "In the office for...",
+    link: "https://in-the-office-for.web.app/",
+    duration: "#{it_entries} Country in website",
+  },
+]
+
+template = File.read("writeme.md.erb")
+
+content = ERB.new(template, nil, "<>").result
+
+File.write("README.md", content)
+
+puts "README updated."
